3a6fb1445dfd90d45e74f2bf121d64025b085391
Reagan <reagan@email.arizona.edu>
2021-03-09 00:20:08 -0700

    Login Page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css'
+import {Button, Form, Label, Input, FormControl} from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Form className="App">
+        <h1> LOGIN </h1>
+        <Form.Group>
+          <Form.Label>Email Address</Form.Label> 
+          <Form.Control type="email" placeholder="Email"/>
+        </Form.Group>
+        <Form.Group>
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+    </Form>
   );
 }
 
