9d00ef22b3c3bbbd87d3ffad943c632526ade57a
Reagan <reagan@email.arizona.edu>
2021-03-25 12:46:02 -0700

    New

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.css'
 import Home from './home';
 import Login from './login';
+import Quiz from './quiz';
+import Score from './score.js';
 import { HashRouter, Switch, Route } from 'react-router-dom';
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
       <Switch>
         <Route exact path ="/" component= { Login } />
         <Route path="/home" component={ Home } />
+        <Route path="/quiz.js" component={ Quiz } />
+        <Route path="/score.js" component = { Score } />
       </Switch>
    </HashRouter>
   );
